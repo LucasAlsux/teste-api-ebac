16b7f60c222a8cf04d6a942f84b0060c9e378abc
Lucas Alsux <lucas.alsux@gmail.com>
2025-05-18 21:47:18 -0300

    Enviando exercicio do módulo 14 para o reposiório na nuvem

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -1,30 +1,69 @@
 /// <reference types="cypress" />
+import contrato from '../contracts/usuarios.contract'
+
 
 describe('Testes da Funcionalidade Usuários', () => {
 
   it('Deve validar contrato de usuários', () => {
-    //TODO: 
+    cy.request('usuarios').then((resposta) =>{
+      return contrato.validateAsync(resposta.body)
+    })
   });
 
-  it('Deve listar usuários cadastrados', () => {
-    //TODO: 
+  it('Deve listar usuários cadastrados - GET', () => {
+    cy.request({
+      method: 'GET',
+      url: 'usuarios',
+    }).should((resposta => {
+      expect(resposta.status).to.equal(200)
+    }))
   });
 
   it('Deve cadastrar um usuário com sucesso', () => {
-    //TODO: 
+    cy.cadastrarUsuario('arthur3', 'arthur3@qa.com.br', 'arthutteste', 'true')
+    .should((resposta) =>{
+      expect(resposta.body.message).to.equal('Cadastro realizado com sucesso')
+      expect(resposta.status).to.equal(201)
+    })
   });
 
   it('Deve validar um usuário com email inválido', () => {
-    //TODO: 
+    cy.request({
+      method: 'GET',
+      url: 'usuarios/1bFocrZMAecabOv7',
+      failOnStatusCode: false
+    }) 
+    .should((email) =>{
+      expect(email.body.usuarios).to.not.equal('arthur3@qa.com.br')
+      expect(email.status).to.equal(400 )
+    })
   });
 
   it('Deve editar um usuário previamente cadastrado', () => {
-    //TODO: 
+      cy.request({
+        method: 'PUT',
+        url: 'usuarios/HWGQQuzL4t5dxxbT',
+        body:{
+        "nome": "arthur 1",
+        "email": "arthur@qa.com.br",
+        "password": "arthutteste",
+        "administrador": "true"
+      }
+      }).should((resposta =>{
+        expect(resposta.body.message).to.equal('Registro alterado com sucesso')
+        expect(resposta.status).to.equal(200)
+      }))
   });
 
   it('Deve deletar um usuário previamente cadastrado', () => {
-    //TODO: 
+      cy.request({
+        method: 'DELETE',
+        url:'usuarios/BZaiZlo4i8yYvSjZ'
+    })
+    .should((resposta) =>{
+    expect(resposta.body.message).to.equal('Registro excluído com sucesso')
+    expect(resposta.status).to.equal(201)
   });
 
-
-});
+  });
+})
